d5186b9316a79df207c457242e78943e688c5276
Ritesh Kushwaha <riteshkushwaha497@gmail.com>
2025-05-15 13:15:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { copyFileSync } from 'fs'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync('public/_redirects', 'dist/_redirects')
+      }
+    }
+  ],
 })
